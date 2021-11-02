1403e01cc39f63576aa26dea75805d756167f95c
Kaylee Dao <3597@holbertonschool.com>
2021-11-02 16:53:58 -0700

    Betty

--- main.h
+++ main.h
@@ -9,19 +9,20 @@ char *_itoa(int, char *);
 int printpercent(void);
 int _printf(const char *format, ...);
 
-
-/**
- * struct pr - Struct of Print func
+/*
+ * /**
+ ** struct pr - Struct of Print func
  *
  * @ar: Argument passed
  * @f: The function associated
  */
+
 /*
-typedef struct pr
-{
-	char *ar;
-	void (*f)(va_list);
-} pr_t;
+ *typedef struct pr
+ *{
+ *	char *ar;
+ *	void (*f)(va_list);
+ *} pr_t;
 */
 
 #endif /* MAIN_H */
